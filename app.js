a9c398a04dd7df3a2145588c3f882ca53f1d7319
Erika Nayely <hernandezllamaserikanayely@gmail.com>
2025-08-09 14:39:18 -0600

    actualizacion con carpeta img incluida para que las imagenes aparezcan' git branch -M main git push -u origin main --force
    '

---
+++ app.js
@@ -0,0 +1,10 @@
+let respuesta = prompt('¿Cual es tu animal domestico favorito? gato, perro, pajaro...')
+if(respuesta === 'gato') {
+    alert('Tienes un gusto exquisito, miau~');
+} else if(respuesta === 'perro') {
+    alert('SAL DE AQUII, GRRRR!!')
+} else if(respuesta === 'pajaro') {
+    alert('¡Sii! ¡Un total manjarrr!... digo, si, miau~');
+} else {
+    alert('Karen deberia sacarme mas al aire libre para conocerlo, miau~');
+}
